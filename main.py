2b4e6d7005c805b625d32ba0efa5584d6ed6301b
Igor Dias <igordiasth@gmail.com>
2024-09-03 10:20:36 -0400

    add selenium webdriver `scrape.py`

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from scrape import scrape_website
+
 
 st.title("Web Scraper AI")
 url = st.text_input("Enter a Website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping the website")
+    result = scrape_website(url)
+    print(result)
